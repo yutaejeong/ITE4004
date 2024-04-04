c3ce87e47914189b844285289c86b2cea7d66998
유태정 <qmamsm123@naver.com>
2024-04-04 15:56:26 +0900

    implementing the connection to clients

--- assignment2/server.c
+++ assignment2/server.c
@@ -1,147 +1,119 @@
 #include <arpa/inet.h>
+#include <netinet/in.h>
 #include <pthread.h>
+#include <semaphore.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <unistd.h>
 
-#define BOARD_WIDTH 5
-#define BUF_SIZE 1024
-#define CLIENT_NUM 2
+#define BUF_SIZE 100
 
-char board[BOARD_WIDTH][BOARD_WIDTH];
-char saved_message[BUF_SIZE];
-
-pthread_mutex_t mutex;
-
-int draw(char player, int row, int col);
-void error_handling(char *message);
-void *handle_client(void *port);
+void initialize_server(int *serv_sock, in_port_t sin_port);
+void connect_to_clients(int serv_sock, int *clnt_sock);
+void finalize_server(int serv_sock);
 
 int main(int argc, char *argv[]) {
-  int i, serv_sock;
-  struct sockaddr_in serv_adr, clnt_adr;
-  socklen_t clnt_adr_sz;
-  pthread_t thread_id[CLIENT_NUM];
+  int serv_sock, clnt_sock[2];
+  struct sockaddr_in serv_adr;
 
   if (argc != 2) {
-    // 실행 시 커맨드라인: ./server 9091
-    printf("Usage: %s <port>\n", argv[0]);
-    exit(1);
+    printf("Usage: %s <PORT>\n", argv[0]);
+    abort();
   }
 
-  // 서버 소켓 생성
-  serv_sock = socket(PF_INET, SOCK_STREAM, 0);
-  if (serv_sock == -1) {
-    error_handling("socket() error");
+  initialize_server(&serv_sock, htons(atoi(argv[1])));
+
+  connect_to_clients(serv_sock, clnt_sock);
+
+  finalize_server(serv_sock);
+
+  return 0;
+}
+
+/**
+ * Function: initialize_server
+ * ---------------------------
+ * 주어진 포트로 서버를 엽니다.
+ * 오류가 발생하면 프로그램을 종료시킵니다.
+ * serv_sock: 생성된 소켓을 저장할 포인터입니다.
+ * sin_port: 서버를 열 포트 번호입니다.
+ */
+void initialize_server(int *serv_sock, in_port_t sin_port) {
+  struct sockaddr_in serv_adr;
+
+  *serv_sock = socket(PF_INET, SOCK_STREAM, 0);
+  if (*serv_sock == -1) {
+    perror("failed to create an endpoint for communication");
+    abort();
   }
 
-  // 서버 소켓 주소 설정
   memset(&serv_adr, 0, sizeof(serv_adr));
   serv_adr.sin_family = AF_INET;
   serv_adr.sin_addr.s_addr = htonl(INADDR_ANY);
-  serv_adr.sin_port = htons(atoi(argv[1]));
-
-  // 서버 소켓 주소와 서버 소켓 바인딩
-  if (bind(serv_sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr)) == -1) {
-    error_handling("bind() error");
-  }
-  printf("Server is running on port %s\n", argv[1]);
-
-  // 서버 소켓으로 클라이언트 접속 대기
-  if (listen(serv_sock, 5) == -1) {
-    error_handling("listen() error");
+  serv_adr.sin_port = sin_port;
+  if (bind(*serv_sock, (struct sockaddr *)&serv_adr, sizeof(serv_adr)) == -1) {
+    perror("failed to bind a name to a socket");
+    abort();
   }
 
-  // 뮤텍스 초기화
-  pthread_mutex_init(&mutex, NULL);
-
-  // 클라이언트 연결
-  for (i = 0; i < CLIENT_NUM; i++) {
-    clnt_adr_sz = sizeof(clnt_adr);
-    int *clnt_sock = (int *)malloc(sizeof(int));
-    *clnt_sock = accept(serv_sock, (struct sockaddr *)&clnt_adr, &clnt_adr_sz);
-    printf("A client is connected (address: %s:%4d, descriptor: %d)\n", inet_ntoa(clnt_adr.sin_addr), ntohs(clnt_adr.sin_port), *clnt_sock);
-    pthread_create(&thread_id[i], NULL, handle_client, (void *)clnt_sock);
+  if (listen(*serv_sock, 5) == -1) {
+    perror("failed to listen for connections on a socket");
+    abort();
   }
 
-  // 클라이언트 연결 종료 대기
-  for (i = 0; i < CLIENT_NUM; i++) {
-    pthread_join(thread_id[i], NULL);
-  }
-
-  // 뮤텍스 해제
-  pthread_mutex_destroy(&mutex);
-
-  // 서버 소켓 닫기
-  close(serv_sock);
-
-  return 0;
+  printf("Server is initialzed successfully.\n");
 }
 
 /**
- * 클라이언트와 연결
+ * Function: connect_to_clients
+ * -----------------------------
+ * 게임을 시작하기에 앞서 두 클라이언트에 연결을 시도합니다.
+ * 각 클라이언트와 연결된 이후 모두 준비가 완료될 때까지 기다립니다.
+ * 오류가 발생하면 프로그램을 종료시킵니다.
+ * serv_sock: 서버 소켓 디스크립터입니다.
+ * clnt_sock: 연결된 클라이언트의 소켓을 저장할 크기 2의 배열 포인터입니다.
  */
-void *handle_client(void *_clnt_sock) {
-  int clnt_sock = *((int *)_clnt_sock);
-  char message[BUF_SIZE];
-  int str_len;
+void connect_to_clients(int serv_sock, int *clnt_sock) {
+  int i, len;
+  char msg[BUF_SIZE];
+  struct sockaddr_in clnt_adr[2];
+  socklen_t clnt_adr_sz[2];
 
-  // 클라이언트와 메시지 송수신
-  while ((str_len = read(clnt_sock, message, BUF_SIZE)) != 0) {
-    message[str_len - 1] = 0;  // 개행 문자 제거 후 널 문자 추가
-
-    pthread_mutex_lock(&mutex);
-
-    printf("saved_message: %s -> ", saved_message);
-    write(clnt_sock, message, str_len);
-    strcpy(saved_message, message);
-    printf("%s\n", saved_message);
-
-    pthread_mutex_unlock(&mutex);
+  // 두 플레이어에 각각 연결을 시도합니다.
+  for (i = 0; i < 2; i++) {
+    clnt_sock[i] = accept(serv_sock, (struct sockaddr *)clnt_adr + i, clnt_adr_sz + i);
+    if (clnt_sock[i] == -1) {
+      perror("failed to accept a connection on a socket");
+      abort();
+    }
+    printf("Connected: %s:%d\n", inet_ntoa(clnt_adr[i].sin_addr), ntohs(clnt_adr[i].sin_port));
   }
 
-  printf("A client is disconnected (descriptor: %d)\n", clnt_sock);
-  close(clnt_sock);
+  // 두 플레이어가 모두 연결이 되었을 때, 각 플레이어에 메시지를 보냅니다.
+  for (i = 0; i < 2; i++) {
+    if (write(clnt_sock[i], "Connected\0", BUF_SIZE) == -1) {
+      perror("failed to write to the client socket (message: Connected)");
+      abort();
+    };
+  }
 
-  free(_clnt_sock);
-  return NULL;
+  printf("All clients connected.\n");
 }
 
 /**
- * 플레이어가 돌을 놓음
- * 0: 성공
- * 1: 범위를 벗어난 입력
- * 2: 이미 표시된 칸임
+ * Function: finalize_server
+ * -------------------------
+ * 서버를 종료시킵니다.
+ * 오류가 발생하면 프로그램을 종료시킵니다.
+ * serv_sock: 종료할 서버의 소켓 디스크립터입니다.
  */
-int draw(char player, int row, int col) {
-  pthread_mutex_lock(&mutex);
-  if (row < 0 || row >= BOARD_WIDTH || col < 0 || col >= BOARD_WIDTH) {
-    return 1;
-  }
-  if (!board[row][col]) {
-    board[row][col] = player;
-  } else {
-    return 2;
-  }
-  pthread_mutex_unlock(&mutex);
-  return 0;
-}
+void finalize_server(int serv_sock) {
+  if (close(serv_sock) == -1) {
+    perror("failed to close the socket");
+    abort();
+  };
 
-/**
- * 게임이 끝났는지 판단
- * 0: 게임이 끝나지 않음
- * 1: 플레이어 1 승리
- * 2: 플레이어 2 승리
- * 3: 무승부
- */
-int determine() {
-  return 0;
-}
-
-void error_handling(char *message) {
-  fputs(message, stderr);
-  fputc('\n', stderr);
-  exit(1);
+  printf("Server is finalized successfully.\n");
 }
