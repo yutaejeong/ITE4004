5a0929193d4827b804772d90f1c035428894664d
유태정 <qmamsm123@naver.com>
2024-04-09 11:28:41 +0900

    toggle color mde when it is light mode

--- assignment3/discord-front/src/App.tsx
+++ assignment3/discord-front/src/App.tsx
@@ -1,9 +1,17 @@
-import { useState } from "react";
+import { useColorMode } from "@chakra-ui/react";
+import { useEffect, useState } from "react";
 import { LoginContainer } from "./containers/login/LoginContainer";
 import { RoomContainer } from "./containers/room/RoomContainer";
 
 function App() {
+  const { colorMode, toggleColorMode } = useColorMode();
   const [nickname, setNickname] = useState<string>("");
+
+  useEffect(() => {
+    if (colorMode === "light") {
+      toggleColorMode();
+    }
+  }, [colorMode, toggleColorMode]);
 
   return nickname ? (
     <RoomContainer nickname={nickname} />
