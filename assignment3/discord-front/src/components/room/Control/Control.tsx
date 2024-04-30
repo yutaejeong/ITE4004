e06c76b4fadf835de8345baa17fc08c3b6aefbe1
유태정 <qmamsm123@naver.com>
2024-05-01 00:37:30 +0900

    레이아웃 스켈레톤 수정

--- assignment3/discord-front/src/components/room/Control/Control.tsx
+++ assignment3/discord-front/src/components/room/Control/Control.tsx
@@ -1,6 +1,13 @@
-import { Card } from "@chakra-ui/react";
+import { Card, CardBody, CardHeader, Heading } from "@chakra-ui/react";
 import "./Control.css";
 
 export function Control() {
-  return <Card className="control-card"></Card>;
+  return (
+    <Card className="control-card">
+      <CardHeader>
+        <Heading size="md">Controls</Heading>
+      </CardHeader>
+      <CardBody>{/* @todo */}</CardBody>
+    </Card>
+  );
 }
