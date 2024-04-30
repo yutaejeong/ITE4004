68769ee8db95b1b791b8e6a98c0c139c9fa95c78
유태정 <qmamsm123@naver.com>
2024-05-01 01:49:17 +0900

    백엔드 모듈 분리

---
+++ assignment3/discord-back/src/camera.ts
@@ -0,0 +1,16 @@
+import { WebSocketServer } from "ws";
+import WebSocket = require("ws");
+
+export const ws_camera = new WebSocketServer({ noServer: true });
+
+ws_camera.on("connection", function connection(ws) {
+  ws.on("error", console.error);
+
+  ws.on("message", function message(data, isBinary) {
+    ws_camera.clients.forEach(function each(client) {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+});
