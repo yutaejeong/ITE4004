83b91dad8674ea803af508ab3434cbb98f88a383
유태정 <qmamsm123@naver.com>
2024-05-19 11:39:00 +0900

    음성 채팅 기능 추가

--- assignment3/discord-back/src/voice.ts
+++ assignment3/discord-back/src/voice.ts
@@ -1,20 +1,100 @@
 import { WebSocketServer } from "ws";
 import WebSocket = require("ws");
 
-export function create_websocket_voice() {
-  const ws_voice = new WebSocketServer({ noServer: true });
+interface Participant {
+  id: string;
+  nickname: string;
+  isVoiceOn: boolean;
+}
 
-  ws_voice.on("connection", function connection(ws) {
-    ws.on("error", console.error);
+export type Message =
+  | {
+      _type: "welcome";
+      id: string;
+      participants: Participant[];
+    }
+  | {
+      _type: "introduce";
+      id: string;
+      nickname: string;
+    }
+  | {
+      _type: "newbie";
+      newbie: Participant;
+    }
+  | {
+      _type: "goodbye";
+      escapee: Participant;
+    };
 
-    ws.on("message", function message(data, isBinary) {
-      ws_voice.clients.forEach(function each(client) {
-        if (client.readyState === WebSocket.OPEN) {
-          client.send(data, { binary: isBinary });
+export class VoiceWebSocketServer {
+  ws_voice: WebSocketServer;
+  participants: Record<string, Participant> = {};
+
+  constructor() {
+    this.ws_voice = new WebSocketServer({ noServer: true });
+
+    this.ws_voice.on("connection", (ws) => {
+      const { v4: uuidv4 } = require("uuid");
+      const clientId = uuidv4();
+
+      const welcomeMessage: Message = {
+        _type: "welcome",
+        id: clientId,
+        participants: Object.values(this.participants),
+      };
+      ws.send(JSON.stringify(welcomeMessage));
+
+      this.participants[clientId] = {
+        isVoiceOn: false,
+        nickname: "",
+        id: clientId,
+      };
+
+      const introduceNewbie = () => {
+        const newbieMessage: Message = {
+          _type: "newbie",
+          newbie: this.participants[clientId],
+        };
+        this.ws_voice.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(newbieMessage));
+          }
+        });
+      };
+
+      ws.on("error", console.error);
+
+      ws.on("message", (data, isBinary) => {
+        if (!isBinary) {
+          const message = JSON.parse(data.toString()) as Message;
+          switch (message._type) {
+            case "introduce":
+              this.participants[message.id].nickname = message.nickname;
+              introduceNewbie();
+              break;
+            default:
+              this.ws_voice.clients.forEach((client) => {
+                if (client.readyState === WebSocket.OPEN) {
+                  client.send(data, { binary: isBinary });
+                }
+              });
+          }
         }
       });
-    });
-  });
 
-  return ws_voice;
+      ws.on("close", () => {
+        const goodbyeMessage: Message = {
+          _type: "goodbye",
+          escapee: this.participants[clientId],
+        };
+        this.ws_voice.clients.forEach((client) => {
+          if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify(goodbyeMessage));
+          }
+        });
+        delete this.participants[clientId];
+      });
+    });
+  }
 }
--- assignment3/discord-front/src/components/room/VoiceSharing/VoiceSharing.tsx
+++ assignment3/discord-front/src/components/room/VoiceSharing/VoiceSharing.tsx
@@ -7,13 +7,236 @@ import {
   StackDivider,
 } from "@chakra-ui/react";
 import { useAtomValue } from "jotai";
+import { useEffect, useRef, useState } from "react";
+import { channelAtom } from "../../../atoms/channel";
 import { voiceConfigAtom } from "../../../atoms/control";
+import { userAtom } from "../../../atoms/user";
 import "./VoiceSharing.css";
+
+interface Participant {
+  id: string;
+  nickname: string;
+  isVoiceOn: boolean;
+}
+
+type Message =
+  | {
+      _type: "audio";
+      sender_id: string;
+      data: string;
+      mime: string;
+    }
+  | {
+      _type: "hide";
+      sender_id: string;
+    }
+  | {
+      _type: "show";
+      sender_id: string;
+    }
+  | {
+      _type: "welcome";
+      id: string;
+      participants: Participant[];
+    }
+  | {
+      _type: "introduce";
+      id: string;
+      nickname: string;
+    }
+  | {
+      _type: "newbie";
+      newbie: Participant;
+    }
+  | {
+      _type: "goodbye";
+      escapee: Participant;
+    };
+
+function blobToBase64(blob: Blob): Promise<string> {
+  return new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.readAsDataURL(blob);
+    reader.onloadend = () => {
+      const base64data = reader.result as string;
+      resolve(base64data);
+    };
+    reader.onerror = (error) => {
+      reject(error);
+    };
+  });
+}
+
+function base64ToBlob(base64: string, mimeType: string): Blob {
+  const byteString = atob(base64.split(",")[1]);
+  const arrayBuffer = new ArrayBuffer(byteString.length);
+  const intArray = new Uint8Array(arrayBuffer);
+  for (let i = 0; i < byteString.length; i++) {
+    intArray[i] = byteString.charCodeAt(i);
+  }
+  return new Blob([arrayBuffer], { type: mimeType });
+}
+
+function playAudio(base64: string): Promise<void> {
+  const audio = new Audio(base64);
+  audio.onended = () => {
+    URL.revokeObjectURL(audio.src);
+  };
+  audio.onerror = (error) => {
+    console.error("cannot load audio: ", error);
+  };
+  return audio.play().catch((error) => {
+    console.error("cannot play audio: ", error);
+  });
+}
 
 export function VoiceSharing() {
   const isVoiceOn = useAtomValue(voiceConfigAtom);
+  const wsRef = useRef<WebSocket | null>(null);
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null);
+  const channel_id = useAtomValue(channelAtom);
+  const { nickname } = useAtomValue(userAtom);
+  const [participants, setParticipants] = useState<Participant[]>([]);
+  const idRef = useRef<string | null>(null);
+  const chunks = useRef<Blob[]>([]);
 
-  console.log(isVoiceOn);
+  useEffect(() => {
+    const endpoint = `${process.env.REACT_APP_WS_SERVER!}/${channel_id}/voice`;
+    const ws = new WebSocket(endpoint);
+
+    wsRef.current = ws;
+
+    ws.onmessage = async (event) => {
+      const message = JSON.parse(event.data) as Message;
+      switch (message._type) {
+        case "welcome":
+          idRef.current = message.id;
+          setParticipants(message.participants);
+          const introduceMessage: Message = {
+            _type: "introduce",
+            id: message.id,
+            nickname,
+          };
+          ws.send(JSON.stringify(introduceMessage));
+          break;
+        case "newbie":
+          setParticipants((prev) => [...prev, message.newbie]);
+          break;
+        case "audio":
+          const blob = base64ToBlob(message.data, message.mime);
+          const url = URL.createObjectURL(blob);
+          playAudio(url).finally(() => {
+            URL.revokeObjectURL(url);
+          });
+          document
+            .querySelectorAll(`.voice-${message.sender_id}`)
+            .forEach((el) => {
+              el.classList.add("voice-active");
+              setTimeout(() => {
+                el.classList.remove("voice-active");
+              }, 1000);
+            });
+          break;
+        case "hide":
+          setParticipants((prev) =>
+            prev.map((participant) =>
+              participant.id === message.sender_id
+                ? ({ ...participant, isVoiceOn: false } satisfies Participant)
+                : participant,
+            ),
+          );
+          break;
+        case "show":
+          setParticipants((prev) =>
+            prev.map((participant) =>
+              participant.id === message.sender_id
+                ? ({ ...participant, isVoiceOn: true } satisfies Participant)
+                : participant,
+            ),
+          );
+          break;
+        case "goodbye":
+          setParticipants((prev) =>
+            prev.filter((participant) => participant.id !== message.escapee.id),
+          );
+          break;
+      }
+    };
+
+    return () => {
+      if (ws.readyState !== WebSocket.CLOSED) {
+        ws.close();
+      }
+    };
+  }, [channel_id, nickname]);
+
+  // Turn on the mic and deliver the voice through the websocket (wsRef.current) every 1000 / 30 ms.
+  async function handleVoiceOn() {
+    const stream = await navigator.mediaDevices.getUserMedia({
+      audio: true,
+    });
+    mediaRecorderRef.current = new MediaRecorder(stream);
+    mediaRecorderRef.current.ondataavailable = (event) => {
+      chunks.current.push(event.data);
+    };
+    mediaRecorderRef.current.onstop = async () => {
+      const blob = new Blob(chunks.current, { type: chunks.current[0].type });
+      chunks.current = [];
+
+      if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
+        const base64String = await blobToBase64(blob);
+        const message: Message = {
+          sender_id: idRef.current!,
+          data: base64String,
+          mime: blob.type,
+          _type: "audio",
+        };
+        wsRef.current?.send(JSON.stringify(message));
+      }
+
+      mediaRecorderRef.current?.start();
+      setTimeout(() => {
+        mediaRecorderRef.current?.stop();
+      }, 500);
+    };
+    mediaRecorderRef.current.start();
+    setTimeout(() => {
+      mediaRecorderRef.current?.stop();
+    }, 500);
+
+    if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
+      const showMessage: Message = {
+        sender_id: idRef.current!,
+        _type: "show",
+      };
+      wsRef.current?.send(JSON.stringify(showMessage));
+    }
+  }
+
+  async function handleVoiceOff() {
+    if (mediaRecorderRef.current) {
+      mediaRecorderRef.current.stop();
+      mediaRecorderRef.current.stream.getTracks().forEach((track) => {
+        track.stop();
+      });
+      mediaRecorderRef.current = null;
+    }
+    if (wsRef.current && wsRef.current.readyState === WebSocket.OPEN) {
+      const hideMessage: Message = {
+        sender_id: idRef.current!,
+        _type: "hide",
+      };
+      wsRef.current?.send(JSON.stringify(hideMessage));
+    }
+  }
+
+  useEffect(() => {
+    if (isVoiceOn) {
+      handleVoiceOn();
+    } else {
+      handleVoiceOff();
+    }
+  }, [isVoiceOn]);
 
   return (
     <Card className="voice-card">
@@ -22,7 +245,12 @@ export function VoiceSharing() {
       </CardHeader>
       <CardBody>
         <Stack divider={<StackDivider />} spacing={4}>
-          {/* @todo */}
+          {participants.map((participant) => (
+            <div className={`voice-${participant.id}`} key={participant.id}>
+              {participant.nickname}
+              {participant.isVoiceOn ? "🔊" : "🔇"}
+            </div>
+          ))}
         </Stack>
       </CardBody>
     </Card>
